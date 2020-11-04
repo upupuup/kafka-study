1feaa9678e9e00a7fd6c1c8128a8254e44b5344c
upupuup <woyaojiayou_a@163.com>
2020-11-04 23:11:00 +0800

    commit

--- src/main/java/com/and/kafka/admin/AdminSample.java
+++ src/main/java/com/and/kafka/admin/AdminSample.java
@@ -2,8 +2,10 @@ package com.and.kafka.admin;
 
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.config.ConfigResource;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @Description: [java类作用描述]
@@ -14,14 +16,43 @@ public class AdminSample {
 
     static final String TOPIC_NAME = "AND_TOPIC";
     static final String IP = "47.102.115.74:9092";
+    static final String PREALLOCATE = "preallocate";
 
     public static void main(String[] args) throws Exception {
 //        AdminClient adminClient = AdminSample.adminClient();
 //        System.out.println("adminClient" + adminClient);
 
+        // 创建实例
 //        createTopic();
+        // 删除实例
+//        delTopics();
 
-        topicList();
+        // 修改配置
+//        alterConfig();
+        // 查看所有实例
+//        topicList();
+        // 增加partitions
+        incrPartitions(2);
+        // 查看topic具体信息
+        describeTopics();
+        // 查看配置信息
+//        describeConfig();
+
+    }
+
+    /**
+     * 增加partitions
+     * @param partitions
+     * @throws Exception
+     */
+    public static void incrPartitions(int partitions) throws Exception {
+        AdminClient adminClient = adminClient();
+        Map<String, NewPartitions> partitionsMap = new HashMap<>(16);
+        NewPartitions newPartitions = NewPartitions.increaseTo(partitions);
+        partitionsMap.put(TOPIC_NAME, newPartitions);
+
+        CreatePartitionsResult createPartitionsResult = adminClient.createPartitions(partitionsMap);
+        createPartitionsResult.all().get();
     }
 
     /**
@@ -33,6 +64,71 @@ public class AdminSample {
         NewTopic newTopic = new NewTopic(TOPIC_NAME, 1, rs);
         CreateTopicsResult topics = adminClient.createTopics(Arrays.asList(newTopic));
         System.out.println("create topics result:" + topics);
+    }
+
+    /**
+     * 修改配置
+     * @throws Exception
+     */
+    public static void alterConfig() throws Exception {
+        AdminClient adminClient = adminClient();
+
+        // 过期的方式
+//        HashMap<ConfigResource, Config> configMaps = new HashMap<>(16);
+//
+//        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, TOPIC_NAME);
+//        Config config = new Config(Arrays.asList(new ConfigEntry(PREALLOCATE, "true")));
+//        configMaps.put(configResource, config);
+//
+//        AlterConfigsResult alterConfigsResult = adminClient.alterConfigs(configMaps);
+
+        // 现在的方式
+        HashMap<ConfigResource, Collection<AlterConfigOp>> configMaps = new HashMap<>(16);
+
+        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, TOPIC_NAME);
+        AlterConfigOp alterConfigOp = new AlterConfigOp(new ConfigEntry(PREALLOCATE, "false"), AlterConfigOp.OpType.SET);
+
+        configMaps.put(configResource, Arrays.asList(alterConfigOp));
+        AlterConfigsResult alterConfigsResult = adminClient.incrementalAlterConfigs(configMaps);
+        alterConfigsResult.all().get();
+    }
+
+
+    /**
+     * 查看配置信息
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public static void describeConfig() throws ExecutionException, InterruptedException {
+        AdminClient adminClient = adminClient();
+        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, TOPIC_NAME);
+        DescribeConfigsResult describeConfigsResult = adminClient.describeConfigs(Arrays.asList(configResource));
+        Map<ConfigResource, Config> configResourceConfigMap = describeConfigsResult.all().get();
+        configResourceConfigMap.entrySet().stream().forEach((entry) -> {
+            System.out.println("configResource:" + entry.getKey() + ",config:" + entry.getValue());
+        });
+
+    }
+
+    /**
+     * 查看topic描述
+     */
+    public static void describeTopics() throws ExecutionException, InterruptedException {
+        AdminClient adminClient = adminClient();
+        DescribeTopicsResult describeTopicsResult = adminClient.describeTopics(Arrays.asList(TOPIC_NAME));
+        Map<String, TopicDescription> stringTopicDescriptionMap = describeTopicsResult.all().get();
+        Set<Map.Entry<String, TopicDescription>> entries = stringTopicDescriptionMap.entrySet();
+        entries.stream().forEach(System.out::println);
+    }
+
+    /**
+     * 删除topic
+     * @throws Exception
+     */
+    public static void delTopics() throws Exception {
+        AdminClient adminClient = adminClient();
+        DeleteTopicsResult deleteTopicsResult = adminClient.deleteTopics(Arrays.asList(TOPIC_NAME));
+        deleteTopicsResult.all().get();
     }
 
     /**
---
+++ src/main/java/com/and/kafka/producer/ProducerSample.java
@@ -0,0 +1,116 @@
+package com.and.kafka.producer;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @Description: [java类作用描述]
+ * @Author: jiangzhihong
+ * @CreateDate: 2020/11/4 21:49
+ */
+public class ProducerSample {
+    static final String TOPIC_NAME = "AND_TOPIC";
+    static final String IP = "47.102.115.74:9092";
+    static final String PREALLOCATE = "preallocate";
+
+    public static void main(String[] args) throws Exception {
+        // producer异步发送演示
+//        producerSend();
+
+        // 同步发送,异步阻塞发送
+//        producerSyncSend();
+
+        // producer异步发送带回调函数演示
+        producerSendWithCallBack();
+    }
+
+    /**
+     * 同步发送,异步阻塞发送
+     */
+    private static void producerSyncSend() throws Exception {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, "0");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Producer的主对象
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 消息对象 - ProducerRecord
+        for (int i = 0; i < 10; i++) {
+            String key = "key-" + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "key-" + i, "value-" + i);
+            Future<RecordMetadata> send = producer.send(record);
+            RecordMetadata recordMetadata = send.get();
+            System.out.println(key + ",partitions:" + recordMetadata.partition() + ",offset:" + recordMetadata.offset());
+        }
+
+        // 所有的通道都需要关闭
+        producer.close();
+    }
+
+    /**
+     * producer异步发送带回调函数演示
+     */
+    public static void producerSendWithCallBack() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, "0");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Producer的主对象
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 消息对象 - ProducerRecord
+        for (int i = 0; i < 10; i++) {
+            String key = "key-" + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "key-" + i, "value-" + i);
+            producer.send(record, (recordMetadata, e) -> {
+                System.out.println(key + ",partitions:" + recordMetadata.partition() + ",offset:" + recordMetadata.offset());
+            });
+        }
+
+        // 所有的通道都需要关闭
+        producer.close();
+    }
+
+    /**
+     * producer异步发送演示
+     */
+    public static void producerSend() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IP);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, "0");
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Producer的主对象
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 消息对象 - ProducerRecord
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "key-" + i, "value-" + i);
+            producer.send(record);
+        }
+
+        // 所有的通道都需要关闭
+        producer.close();
+    }
+}
